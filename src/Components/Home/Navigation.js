1310339098c13bd1ec219ef07c7985ff3e88b10c
kanishq1 <kanishqkhandelwal@gmail.com>
2021-06-16 12:56:59 +0530

    vertical scroll sorted

--- src/Components/Home/Navigation.js
+++ src/Components/Home/Navigation.js
@@ -4,12 +4,10 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import Button from '@material-ui/core/Button';
-
 const useStyles = makeStyles((theme) => ({
 	root: {
 		flexGrow: 1,
@@ -19,6 +17,7 @@ const useStyles = makeStyles((theme) => ({
 	},
 	title: {
 		flexGrow: 1,
+		textAlign: 'center',
 	},
 }));
 
@@ -39,9 +38,9 @@ export default function MenuAppBar() {
 		<div className={classes.root}>
 			<AppBar position="static">
 				<Toolbar>
-					<IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+					{/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
 						<MenuIcon />
-					</IconButton>
+					</IconButton> */}
 					<Button style={{ color: 'white' }} onClick={() => {}}>
 						SAVE
 					</Button>
